456caf85aee9c112b9d3262b29996e963b886d2c
Thomas Pentenrieder <pentenrieder@medialesson.de>
2017-10-29 00:42:03 +0200

    sample file update

--- WordPressUWP/Helpers/ApiCredentials.sample.cs
+++ WordPressUWP/Helpers/ApiCredentials.sample.cs
@@ -4,6 +4,7 @@
     // public static class ApiCredentials
     public static class ApiCredentialsSample
     {
+        public const string BaseUri = "http://yoursite.com/";
         public static string WordPressUri = "http://yoursite.com/wp-json/";
     }
 }
